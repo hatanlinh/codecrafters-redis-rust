03917706598c39fc630330f41e40dbffeaeec34a
hatanlinh <linh@fseek.net>
2026-06-01 20:43:19 +0700

    feat: respond to ping [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-#![allow(unused_imports)]
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn handle_connection(mut stream: TcpStream) {
+    stream.write("+PONG\r\n".as_bytes()).unwrap();
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
+    for connection in listener.incoming() {
+        match connection {
+            Ok(stream) => {
                 println!("accepted new connection");
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
