5dda5f4eb54fbfe1962756c20b0e36ca01b277b7
hatanlinh <linh@fseek.net>
2026-06-01 21:00:30 +0700

    feat: respond to multiple pings [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,21 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 
 fn handle_connection(mut stream: TcpStream) {
-    stream.write("+PONG\r\n".as_bytes()).unwrap();
+    let mut buf = [0u8; 256];
+    loop {
+        match stream.read(&mut buf) {
+            Ok(0) => {
+                break;
+            }
+            Ok(_bytes_read) => {
+                stream.write_all("+PONG\r\n".as_bytes()).unwrap();
+            }
+            Err(e) => {
+                println!("error: {}", e);
+                break;
+            }
+        }
+    }
 }
 
 fn main() {
